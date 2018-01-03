e31ee9ccbf0b8522782ef1b96bcc3ae703531200
Yuki Ito <acomagu@gmail.com>
2018-01-03 20:33:12 +0900

    [Add] src

---
+++ conf.d/__async_prompt.fish
@@ -0,0 +1,53 @@
+function __async_prompt_setup --on-event fish_prompt
+    functions -e (status current-function)
+
+    functions -q fish_prompt
+    and function fish_prompt
+        __async_prompt __async_prompt_text
+    end
+
+    functions -q fish_right_prompt
+    and function fish_right_prompt
+        __async_prompt __async_prompt_right_text
+    end
+end
+
+function __async_prompt
+    set pref $argv[1]
+    set univ_text_name $pref'_'(echo %self)
+    set univ_text (eval 'echo $'$univ_text_name)
+    set glbl_text_name $pref
+    set glbl_text (eval 'echo $'$glbl_text_name)
+
+    set -q $univ_text_name; and begin
+        echo $univ_text
+        set -g $glbl_text_name $univ_text
+        set -e $univ_text_name
+    end; or begin
+        echo $glbl_text
+    end
+end
+
+function __async_prompt_fire --on-event fish_prompt
+    functions -q fish_prompt
+    and __async_prompt_ask __async_prompt
+
+    functions -q fish_right_prompt
+    and __async_prompt_ask __async_prompt_right
+end
+
+function __async_prompt_ask
+    set pref $argv[1]
+    fish -c $pref'_wrapper '(echo %self) &
+    function $pref'_handler' --on-process-exit (jobs -lp | tail -n1)
+        kill -WINCH %self
+    end
+end
+
+function __async_prompt_wrapper
+    set -U __async_prompt_text_$argv[1] (fish_prompt)
+end
+
+function __async_prompt_right_wrapper
+    set -U __async_prompt_right_text_$argv[1] (fish_right_prompt)
+end
