63f6cc191bcac1b112135786ba94940b9e56248d
Kim Streich <kim@infusedinsight.com>
2025-01-02 00:24:59 -0600

    Revert "Fix #69" to fix status forwarding not working anymore
    This was supposed to fix issue:
    https://github.com/acomagu/fish-async-prompt/issues/69
    
    But the comment from the OP shows that it didn’t actually fix it and instead broke the status variable forwarding.
    
    Therefore, I am reverting it.
    
    This reverts commit 23e416fdece97c388173ba2c0065a10d185a0821.

--- conf.d/__async_prompt.fish
+++ conf.d/__async_prompt.fish
@@ -18,13 +18,11 @@ function __async_prompt_setup_on_startup --on-event fish_prompt
     end
 end
 
-function __async_prompt_keep_last_pipestatus
-    set -g __async_prompt_last_pipestatus $pipestatus
-end
-
 not set -q async_prompt_on_variable
 and set async_prompt_on_variable fish_bind_mode
 function __async_prompt_fire --on-event fish_prompt (for var in $async_prompt_on_variable; printf '%s\n' --on-variable $var; end)
+    set -l __async_prompt_last_pipestatus $pipestatus
+
     for func in (__async_prompt_config_functions)
         set -l tmpfile $__async_prompt_tmpdir'/'$fish_pid'_'$func
 
@@ -33,7 +31,7 @@ function __async_prompt_fire --on-event fish_prompt (for var in $async_prompt_on
             eval (string escape -- $func'_loading_indicator' "$last_prompt") >$tmpfile
         end
 
-        __async_prompt_config_inherit_variables | __async_prompt_spawn \
+        __async_prompt_config_inherit_variables | __async_prompt_last_pipestatus=$__async_prompt_last_pipestatus __async_prompt_spawn \
             $func' | read -z prompt
             echo -n $prompt >'$tmpfile
     end
